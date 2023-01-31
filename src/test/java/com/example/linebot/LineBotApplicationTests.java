800de3aa0233b31a392e82dbcc75d6f8f92037a1
Harvey <kyo6108@gmail.com>
2023-01-31 22:45:55 +0800

    test

---
+++ src/test/java/com/example/linebot/LineBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
